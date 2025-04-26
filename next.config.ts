d367c3f4a5c2d7734590fe240ceb70fdf32b32dd
Ange230700 <kouakouangeericstephane@gmail.com>
2025-04-26 16:58:03 +0200

    fix: 🐛  repair config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,15 @@
 
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    resolveAlias: {
+      "next/dist/esm/server/lib/lru-cache": "next/dist/server/lib/lru-cache",
+      "next/dist/esm/server/lib/patch-fetch":
+        "next/dist/server/lib/patch-fetch",
+    },
+  },
 };
 
 export default nextConfig;
